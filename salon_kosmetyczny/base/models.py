b8449b465c179cf0eb279531a1194f72f40705c0
goalb <91460691+goalb@users.noreply.github.com>
2022-07-27 13:03:52 +0200

    Add files via upload

--- salon_kosmetyczny/base/models.py
+++ salon_kosmetyczny/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    email = models.EmailField(max_length=200)
+    first_name = models.CharField('first_name', max_length=50, null=True)
+    last_name = models.CharField('last_name', max_length=60, null=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
--- salon_kosmetyczny/base/views.py
+++ salon_kosmetyczny/base/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home.html', {})
+
+
+# Navbar things
+def treatments(request):
+    return render(request, 'treatments.html', {})
+
+
+def prices(request):
+    return render(request, 'prices.html', {})
+
+
+def reviews(request):
+    return render(request, 'reviews.html', {})
+
+
+def sale(request):
+    return render(request, 'sale.html', {})
+
+
+def training(request):
+    return render(request, 'training.html', {})
+
+
+def team(request):
+    return render(request, 'team.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def appointment(request):
+    return render(request, 'appointment.html', {})
+# End navbar
